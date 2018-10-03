2e1977ab64769118430a387e9f2dea65afe37ef7
moises dias <moises.dias16@outlook.com>
2018-10-03 09:38:32 -0300

    finalizado primeira inserção e consulta db

--- src/br/com/caelum/jdbc/teste/TesteConexao.java
+++ src/br/com/caelum/jdbc/teste/TesteConexao.java
@@ -1,15 +1,26 @@
 package br.com.caelum.jdbc.teste;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-
-import br.com.caelum.jdbc.ConnectionFactory;
+import java.util.Calendar;
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
 
 public class TesteConexao {
 	public static void main(String[] args) throws SQLException {
-		Connection connection = new ConnectionFactory().getConnection();
-		System.out.println("Conexão aberta!");
-		connection.close();
+		
+		// pronto para gravar
+		Contato contato = new Contato();
+		contato.setNome("Caelum");
+		contato.setEmail("contato@caelum.com.br");
+		contato.setEndereco("R. Vergueiro 3185 cj57");
+		contato.setDataNascimento(Calendar.getInstance());
+		
+		// grave nessa conexão!!!
+		ContatoDao dao = new ContatoDao();
+		
+		// método elegante
+		dao.adiciona(contato);
+		System.out.println("Gravado!");
 	}
 
 }
